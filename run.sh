c3389563ed686c38f5e3c9d8e272d33812e7da7b
Frodo <xfr@outlook.com>
2023-01-06 11:49:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
 ./src/gmediarender -f gmrender-resurrect --logfile=stdout --gstout-videosink=fakesink --gstout-audiosink=fakesink
+
+exit 0
+
+VLC/3.0.18
+%s LibVLC/3.0.18
+
+curl/7.87.
+%s8 aria2/1.36.6
